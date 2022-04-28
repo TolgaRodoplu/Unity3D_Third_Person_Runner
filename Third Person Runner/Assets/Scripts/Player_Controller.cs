4c3f72e938b4bc278199f2d59e5b847a324516d5
Tolga Rodoplu <Rodoplutolga@gmail.com>
2022-04-28 04:02:31 +0300

    Designing the level

--- Third Person Runner/Assets/Scripts/Player_Controller.cs
+++ Third Person Runner/Assets/Scripts/Player_Controller.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+    Rigidbody rb { get { return GetComponent<Rigidbody>(); } }
+    float forward_speed = 5f;
+    float horizontal_speed = 0.5f;
+    Vector3 move;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var axis = Input.GetAxis("Horizontal");
+        var horizontal_vect = transform.right * horizontal_speed * axis;
+        var vertical_vect = transform.forward * forward_speed;
+        move = transform.position + (horizontal_vect + vertical_vect * Time.fixedDeltaTime);
     }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(move);    
+    }
+
 }
